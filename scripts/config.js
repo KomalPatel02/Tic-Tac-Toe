67dc6d0e1370692f7c0a7b8a924314f02f2457b4
KomalPatel02 <kotadiyakomal532@gmail.com>
2024-02-09 16:40:24 +0530

    Validating User Input With JavaScript

--- scripts/config.js
+++ scripts/config.js
@@ -6,11 +6,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
 }
 
 function savePlayerConfig() {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredplayername = formData.get("playername");
-  console.log(enteredplayername);
+  const enteredplayername = formData.get("playername").trim(); // '     ' => ''
+
+  if (!enteredplayername) {
+    // enteredplayername === ''
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 }
